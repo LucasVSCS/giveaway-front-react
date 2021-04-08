60b171749c4d47388b55acf18e693004395885d8
Lucas Vinicius <lucasvscs@outlook.com>
2021-04-08 00:14:06 -0300

    Começando a fazer o delete do usuário

--- src/components/atoms/Dashboard/UserTableColumns.js
+++ src/components/atoms/Dashboard/UserTableColumns.js
@@ -1,6 +1,13 @@
+import React from 'react'
+import EditIcon from '@material-ui/icons/Edit'
+import Box from '@material-ui/core/Box'
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever'
+import IconButton from '@material-ui/core/IconButton'
+import Button from '@material-ui/core/Button'
+
 const columns = [
-  { field: 'id', headerName: 'ID', width: 150 },
-  { field: 'name', headerName: 'Nome', width: 150 },
+  { field: 'id', headerName: 'ID', width: 80 },
+  { field: 'name', headerName: 'Nome', width: 220 },
   {
     field: 'beginning_period',
     headerName: 'Inicio periodo de uso',
@@ -18,7 +25,33 @@ const columns = [
     headerName: 'Permissão de sorteio',
     width: 190
   },
-  { field: 'user_type', headerName: 'Tipo de usuário', width: 160 }
+  { field: 'user_type', headerName: 'Tipo de usuário', width: 160 },
+  {
+    field: 'actions',
+    headerAlign: 'center',
+    headerName: 'Ações',
+    width: 150,
+    renderCell: params => (
+      <Box mx='auto'>
+        <IconButton
+          aria-label='edit'
+          onClick={() => {
+            console.log(params.row)
+          }}
+        >
+          <EditIcon />
+        </IconButton>
+        <IconButton
+          aria-label='delete'
+          onClick={() => {
+            console.log(params.row.id)
+          }}
+        >
+          <DeleteForeverIcon />
+        </IconButton>
+      </Box>
+    )
+  }
 ]
 
 export default columns
