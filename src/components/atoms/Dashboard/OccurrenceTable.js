9b5d9b22ffd6bb2c289624ff0dbc9910b3271a99
Lucas Vinicius <lucasvscs@outlook.com>
2021-05-28 10:46:49 -0300

    Adicionando os dados vindo do banco na tabela de ocorrências

--- src/components/atoms/Dashboard/OccurrenceTable.js
+++ src/components/atoms/Dashboard/OccurrenceTable.js
@@ -1,9 +1,10 @@
-import * as React from 'react'
+import React, { Component } from 'react'
 import { DataGrid } from '@material-ui/data-grid'
+import instance from '../../../apis/axios-default'
 
 const columns = [
   { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'description', headerName: 'Descrição', width: 500},
+  { field: 'description', headerName: 'Descrição', width: 500 },
   {
     field: 'occurrence_date',
     headerName: 'Data ocorrência',
@@ -11,25 +12,48 @@ const columns = [
     width: 170
   },
   {
-    field: 'userOccurrence',
+    field: 'name',
     headerName: 'Usuário',
     width: 130
   }
 ]
 
-const rows = [
-  {
-    id: 1,
-    name: 'Snow',
-    occurrence_date: '15/02/2021 15:20:10',
-    userOccurrence: 'Lucas Vinicius'
-  }
-]
+export default class OccurenceTable extends Component {
+  constructor (props) {
+    super(props)
 
-export default function OccurenceTable () {
-  return (
-    <div style={{ width: '100%' }}>
-      <DataGrid rows={rows} columns={columns} pageSize={2} autoHeight={true} />
-    </div>
-  )
+    this.state = {
+      apiResponse: []
+    }
+  }
+
+  componentDidMount () {
+    this.callApi()
+  }
+
+  callApi () {
+    instance
+      .get('occurrences/getOccurrences')
+      .then(response => {
+        this.setState({ apiResponse: response.data })
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  render () {
+    return (
+      <div style={{ width: '100%' }}>
+        <DataGrid
+          rows={this.state.apiResponse}
+          columns={columns}
+          pageSize={2}
+          checkboxSelection={false}
+          disableSelectionOnClick={true}
+          autoHeight={true}
+        />
+      </div>
+    )
+  }
 }
